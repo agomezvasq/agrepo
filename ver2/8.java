224bf17588b130edaebf40b663855cb26ba4cb66
agomezvasq <andresgvasquez@icloud.com>
2015-05-20 23:22:35 -0500

    Updates

--- ver2/8.java
+++ ver2/8.java
@@ -16,12 +16,23 @@ public class Latem extends JLabel {
 
 		this.setFont( new Font("Segoe UI", Font.BOLD, 24) );
 		this.setToolTipText( "{"pa+","+pB+"}" );
+
+		URL ico;
+		
+		switch ( atom.flavor ) {
+			case MapGraph.DRONE: 
+				ico = getClass().getResource("drone.png"); break;
+			case MapGraph.OBSTL:
+				ico = getClass().getResource("orb.png"); break;
+			case MapGraph.SOLUT:
+				ico = getClass().getResource("target.png"); break;
+		}
+
+		if (ico!=null)
+			this.setImageIcon( new ImageIcon(ico).getImage() );
 	}
 
 	public void color()   { this.setBackground(Color.CYAN);  }
 	public void uncolor() { this.setBackground(Color.WHITE); }
 
-	public void draw() {
-
-	}
 }
